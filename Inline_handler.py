6bb3d4af7715c4789064f4094ca44096bb55bb98
christmas-fire <vpiskin335@gmail.com>
2024-05-28 00:15:50 +0700

    Сделал кнопку inline_start

--- Inline_handler.py
+++ Inline_handler.py
@@ -1,4 +1,10 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-
-
+def inline_start() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="Запустить бота", callback_data="start")]
+        ]
+    )
+    return kb
